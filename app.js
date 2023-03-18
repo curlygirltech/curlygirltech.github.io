854d8103529f19bb1d87840877a27f5dd42a8902
jcameron10 <jovonnec@gmail.com>
2023-03-18 18:49:43 -0400

    add files

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("HERE");
+
+// click a button
+// change the background color of the body
+
+// get a reference to the button
+const button = document.getElementById("btn");
+// add an event listener to the button
+button.addEventListener("click", function () {
+  // change the background color of the body
+  document.body.style.backgroundColor = 'red'
+});
+// const text = document.getElementById('clipboard')
+// title.addEventListener('copy', function () {
+  
+// })
